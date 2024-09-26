7a618946363c4d0617d0147792781303872884fa
ray <dwiyandraraysha@gmail.com>
2024-09-26 22:30:22 +0700

    Update SOAL NO 1.cpp

--- SOAL NO 1.cpp
+++ SOAL NO 1.cpp
@@ -4,55 +4,51 @@
 #include <iostream>
 using namespace std;
 
-// Fungsi untuk memeriksa apakah bilangan adalah bilangan prima
-bool isPrime(int num) {
-    if (num < 2) return false; // Bilangan < 2 bukan prima
-    for (int i = 2; i < num; i++) { // Memeriksa dari 2 hingga num-1
-        if (num % i == 0) return false; // Jika habis dibagi, bukan prima
-    }
-    return true; // Jika tidak ada pembagi, maka prima
-}
-
-// Fungsi untuk mencetak faktor dari bilangan
-void printFactors(int num) {
-    cout << "Faktor dari " << num << ": ";
-    for (int i = 1; i <= num; i++) {
-        if (num % i == 0) {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-}
-
-// Fungsi utama
 int main() {
-    int limit;
+    int batas;
 
-    cout << "Masukkan batas angka: ";
-    cin >> limit; // Input batas angka
+    // Input batas dari pengguna
+    cout << "Masukkan batas bilangan: ";
+    cin >> batas;
 
-    // Menampilkan bilangan ganjil
-    cout << "\nBilangan Ganjil: ";
-    for (int i = 1; i <= limit; i += 2) {
-        cout << i << " ";
-        printFactors(i);
-    }
+    // Loop untuk mengecek setiap bilangan dari 1 hingga batas
+    for (int i = 1; i <= batas; i++) {
+        bool prima = true;
 
-    // Menampilkan bilangan genap
-    cout << "\n\nBilangan Genap: ";
-    for (int i = 2; i <= limit; i += 2) {
-        cout << i << " ";
-        printFactors(i);
-    }
-
-    // Menampilkan bilangan prima
-    cout << "\n\nBilangan Prima: ";
-    for (int i = 1; i <= limit; i++) {
-        if (isPrime(i)) {
-            cout << i << " ";
-            printFactors(i);
+        // Cek apakah bilangan ganjil atau genap
+        if (i % 2 != 0) {
+            cout << i << " adalah bilangan ganjil." << endl;
+        } else {
+            cout << i << " adalah bilangan genap." << endl;
         }
+
+        // Cek apakah bilangan prima
+        if (i <= 1) {
+            prima = false;
+        } else {
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+        }
+
+        if (prima) {
+            cout << i << " adalah bilangan prima." << endl;
+        }
+
+        // Menampilkan faktor dari bilangan
+        cout << "Faktor dari " << i << ": ";
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+        cout << "-------------------" << endl;
     }
 
     return 0;
 }
+
